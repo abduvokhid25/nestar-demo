0f3b2ce9e8b3be67195108910d0ec013fd78a81a
Abduvokhid25 <eminovabduvohid@Eminovui-MacBookPro.local>
2026-05-05 18:14:21 +0900

    feat: SAS integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,29 @@
-import { Box, Container, Stack } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import { NextPage } from "next";
 import withLayoutMain from "./libs/components/layout/LayoutHome";
 
 const Home: NextPage = () => {
   return (
     <>
-      <Container>
+      <Stack>
         <Stack flexDirection={"column"}>
-          <Box>Popular Properties</Box>
-          <Box>Top Agents</Box>
-          <Box>Top Properties</Box>
-          <Box>Events</Box>
+          <Stack> 
+            <Stack className="container">Popular Properties</Stack>
+          </Stack>
+
+          <Stack> 
+            <Stack className="container">Top Agents</Stack>
+          </Stack>
+
+          <Stack> 
+            <Stack className="container">Top Properties</Stack>
+          </Stack>
+
+          <Stack> 
+            <Stack className="container">Events</Stack>
+          </Stack>
         </Stack>
-      </Container>
+      </Stack>
     </>
   );
 };
