2de1d6a34bccf46f50fff09e468ff56bb5e2affa
Abduvokhid25 <eminovabduvohid@Eminovui-MacBookPro.local>
2026-05-10 12:57:49 +0900

    feat: develop apollo client connection

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,10 +10,31 @@ import useDeviceDetect from "./libs/hooks/useDeviceDetect";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import { GET_PROPERTIES } from "@/apollo/user/querty";
+import { useQuery } from "@apollo/client";
 
 
 const Home: NextPage = () => {
   const device = useDeviceDetect();
+
+  const {
+  loading: getPropertiesLoading,
+  data: getPropertiesData,
+  error: getPropertiesError,
+  refetch: getPropertiesRefetch,
+} = useQuery(GET_PROPERTIES, {
+  fetchPolicy: "network-only",
+  variables: {
+    input: {
+      page: 1,
+      limit: 5,
+      sort: "createdAt",
+      direction: "DESC",
+      search: {},
+    },
+  },
+});
+console.log( "getPropertiesData =>", getPropertiesData);
 
   if (device === "mobile") {
     return <Stack>HOMEPAGE MOBILE</Stack>;
